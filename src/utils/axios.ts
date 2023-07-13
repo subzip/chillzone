c4819d43ca105f47891422d058b289b51bc41fb1
unknown <subzip1@gmail.com>
2023-07-13 22:20:52 +0300

    fd

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://194.67.116.193:4444",
+  baseURL: "http://194.67.116.193:4444/",
 });
 
 instance.interceptors.request.use((config) => {
